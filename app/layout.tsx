81bd7f6055c13861f4eae1169c10418b33619082
DESKTOP-5670IME\chii8 <chii81020@gmail.com>
2024-10-20 11:15:53 +0900

    next.js移行途中

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>My App</title>
+        <meta name="description" content="My App is a..." />
+      </head>
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // シングルページアプリケーション（SPA）を出力します。
+  distDir: "./dist", // ビルド出力ディレクトリを`./dist/`に変更します。
+};
+
+export default nextConfig;
