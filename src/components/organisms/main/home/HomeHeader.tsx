2fba4dc0f53234e142eacf8f50a8fc6ecc612347
DESKTOP-5670IME\chii8 <chii81020@gmail.com>
2024-11-07 15:13:41 +0900

    ヘッダーを上部に固定

--- src/components/organisms/main/home/HomeHeader.tsx
+++ src/components/organisms/main/home/HomeHeader.tsx
@@ -3,7 +3,7 @@ import { SerchBox } from "../../../molecules/SerchBox";
 
 export const HomeHeader = () => {
   return (
-    <header className="bg-primary-300 border-b shadow-sm border-border-200">
+    <header className="bg-primary-300 border-b shadow-sm border-border-200 sticky top-0 z-40">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2 flex items-center justify-between">
         <h1 className="text-3xl font-bold text-secondary-300">Yeah!</h1>
         <SerchBox />
