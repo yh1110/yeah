e90852947eaae0322dedbea0bf00e75de9816395
DESKTOP-5670IME\chii8 <chii81020@gmail.com>
2024-11-07 22:41:54 +0900

    環境変数の取得の仕方を変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import env from "vite-plugin-env-compatible";
 
 export default defineConfig({
-  plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
