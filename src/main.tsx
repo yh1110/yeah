4486d071d2a777477c3c4bb993483450bf9b577d
DESKTOP-5670IME\chii8 <chii81020@gmail.com>
2024-11-11 23:19:37 +0900

    ローディング画面を表示する遅延レンダリングのみ

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { lazy, StrictMode, Suspense } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.tsx";
 import "./css/index.css";
-const UIComponent = lazy(() => import("./UIComponent.tsx"));
+import UIComponent from "./UIComponent";
+// const UIComponent = lazy(() => import("./UIComponent.tsx"));
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Suspense fallback={<div style={{ backgroundColor: "#121212", height: "100vh" }}></div>}>
-      <UIComponent />
-    </Suspense>
+    <UIComponent />
   </StrictMode>
 );
